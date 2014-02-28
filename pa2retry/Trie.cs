63238bf435b600ac41f896d8c8d6738989730c38
unknown <iguest@is-m440m27-15.ischool.uw.edu>
2014-02-27 21:16:57 -0800

    Initialization

---
+++ pa2retry/Trie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pa2retry
+{
+    public class Trie
+    {
+        public TrieNode root;
+
+        public Trie()
+        {
+            root = new TrieNode(' ');
+        }
+
+        public void insertWord(string word)
+        {
+            foreach (char c in word)
+            {
+                TrieNode node = new TrieNode(c);
+            }
+        }
+
+        private void insertChar(char c)
+        {
+
+        }
+    }
+}
---
+++ pa2retry/TrieNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pa2retry
+{
+    public class TrieNode
+    {
+        public char letter { get; set; }
+        public bool lastChar { get; set; }
+        public Dictionary<char, TrieNode> edge { get; set; }
+
+        public TrieNode(char c)
+        {
+            edge = new Dictionary<char, TrieNode>();
+            this.letter = c;
+        }
+
+        public TrieNode getNext(TrieNode node)
+        {
+            TrieNode nextNode;
+            if (node.edge.TryGetValue(node.letter, out nextNode))
+                return nextNode;
+            else
+                return null;
+        }
+
+        public bool isNext(TrieNode node)
+        {
+            TrieNode nextNode;
+            return 
+        }
+    }
+}
